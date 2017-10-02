4459cec65c472f03141bb1e1c18383198f04eba4
Michael Karpinski <mike.karpinski99@gmail.com>
2017-10-02 09:49:15 -0400

    added comments, simplified request and error handling, and added header to request to prevent error 429

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#import libraries
 import ctypes
 import os
 import requests
@@ -6,26 +7,32 @@ import urllib
 import time
 
 def get_image_url():
+    """if there is no error, return URL of background image from EarthPorn"""
+    #gather information from reddit API
     request_url = 'https://www.reddit.com/r/EarthPorn/top/.json?count=20'
-    result = requests.get(request_url)
+    result = requests.get(request_url, headers = {'User-agent': 'friendly program'})
     response = json.loads(result.text)
-    if response == {'message': 'Too Many Requests', 'error': 429}:
+
+    if "error" in response:
         print(response)
         return 1
     else:
-        url = response["data"]["children"][0]["data"]["preview"]["images"][0]["source"]["url"]
+        url = response["data"]["children"][0]["data"]["url"]
         return url
 
 def download_image(image_url):
+    """download and save image to current directory"""
     f = open('image.jpg','wb')
     f.write(requests.get(image_url).content)
     f.close()
 
 def set_background():
+    """set the desktop background"""
     path = os.getcwd()
     ctypes.windll.user32.SystemParametersInfoW(20, 0, path + '\image.jpg' , 0)
 
 image_url = get_image_url()
+
 if image_url != 1:
     download_image(get_image_url())
     set_background()
