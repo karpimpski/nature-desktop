1463a6631fb2976ffee442d1b84668fd07558fff
Michael Karpinski <mike.karpinski99@gmail.com>
2017-10-06 01:58:50 -0400

    simplified main.py

---
+++ functions.py
@@ -0,0 +1,35 @@
+#import libraries
+import requests
+import json
+
+def image_ratio(image):
+    """return width/height ratio of given image"""
+    width = image['data']['preview']['images'][0]['source']['width']
+    height = image['data']['preview']['images'][0]['source']['height']
+    return width/height
+
+def find_most_compatible(children, platform):
+    """take a list of images and return the most compatible one"""
+    children = sorted(children, key=lambda k: image_ratio(k))
+    most_compatible = min(
+        children, key=lambda x:abs(image_ratio(x)-platform.screen_resolution_ratio())
+    )
+    url = most_compatible['data']['url']
+    return url
+
+def get_image_url(platform):
+    """if there is no error, return URL of background image from EarthPorn"""
+    #gather information from reddit API
+    request_url = 'https://www.reddit.com/r/EarthPorn/top/.json?limit=20'
+    result = requests.get(request_url, headers = {'User-agent': 'friendly program'})
+    response = json.loads(result.text)
+
+    if "error" in response:
+        #print error message if there is an error
+        print('Error ' + str(response["error"]) + '. ' + response["message"])
+        return 1
+    else:
+        #find most compatible image and return it
+        children = response["data"]["children"]
+        url = find_most_compatible(children, platform)
+        return url
--- main.py
+++ main.py
@@ -1,50 +1,17 @@
 #import libraries
-import requests
-import json
 import time
+
+#import general methods module
+import functions
 
 #import platform modules
 from platforms.windows import Windows
-
-def image_ratio(image):
-    """return width/height ratio of given image"""
-    width = image['data']['preview']['images'][0]['source']['width']
-    height = image['data']['preview']['images'][0]['source']['height']
-    return width/height
-
-def find_most_compatible(children):
-    """take a list of images and return the most compatible one"""
-    children = sorted(children, key=lambda k: image_ratio(k))
-    most_compatible = min(
-        children, key=lambda x:abs(image_ratio(x)-platform.screen_resolution_ratio())
-    )
-    url = most_compatible['data']['url']
-    return url
-
-def get_image_url():
-    """if there is no error, return URL of background image from EarthPorn"""
-    #gather information from reddit API
-    request_url = 'https://www.reddit.com/r/EarthPorn/top/.json?limit=20'
-    result = requests.get(request_url, headers = {'User-agent': 'friendly program'})
-    response = json.loads(result.text)
-
-    if "error" in response:
-        #print error message if there is an error
-        print('Error ' + str(response["error"]) + '. ' + response["message"])
-        return 1
-    else:
-        #find most compatible image and return it
-        children = response["data"]["children"]
-        url = find_most_compatible(children)
-        return url
-
-
 
 #determine platform
 platform = Windows()
 
 start_time = time.time()
-image_url = get_image_url()
+image_url = functions.get_image_url(platform)
 
 if image_url != 1:
     platform.download_image(image_url)
