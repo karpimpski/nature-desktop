7b233493d093ce850d0360ad8eec8739830cb8f9
Michael Karpinski <mike.karpinski99@gmail.com>
2017-10-12 12:26:39 -0400

    switched to tkinter dialogs and added better comments in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #import libraries
 import sys
-import easygui
+import tkinter
+from tkinter import messagebox
 
 import config
 import functions
@@ -13,17 +14,27 @@ elif sys.platform == 'linux' or sys.platform == 'linux2':
     from ubuntu import Ubuntu
     platform = Ubuntu()
 
-#run application
-message = 'This application will change your desktop\'s background. Continue?'
+#start and close tkinter root window
+root = tkinter.Tk()
+root.withdraw()
+
+#save prompts
+confirm_message = 'This application will change your desktop\'s background. Continue?'
 save_message = 'The background has been set. Would you like to save the image?'
 
-if easygui.ynbox(message, config.title, ('Yes', 'No')):
+#warn user and confirm
+if messagebox.askyesno(config.title, confirm_message):
+    #retrieve image url
     image_url = functions.get_image_url(platform)
 
+    #test that a url was retrieved
     if image_url != 1:
+        #download and set the background image
         platform.download_image(image_url)
         platform.set_background()
-        if easygui.ynbox(save_message, config.title, ('Yes','No')):
+        
+        #ask user if they would like to save image to disk
+        if messagebox.askyesno(config.title, save_message):
             platform.save_background()
 
 sys.exit()
